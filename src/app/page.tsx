6743442dc0f6c503fa67fe1652fa7bc29e0950df
wibawaarif <arifwb.work@gmail.com>
2024-05-29 09:31:59 +0700

    finishing landing page (except footer)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Reviews } from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -137,14 +139,24 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  "I&apos;am tired of the same old boring case and vulnerable. However, this case feels durable and I even got a compliment on the
-                  design. Had the case for three and a half months now and{" "}
-                  <span className="p-0.5 bg-slate-800 text-white">the image is crystal clear</span>, never had a problem on the case comparing my old case. The image started fading into yellow-ish color after a
-                  couple weeks. Really recommended 👍"
+                  "I&apos;am tired of the same old boring case and vulnerable.
+                  However, this case feels durable and I even got a compliment
+                  on the design. Had the case for three and a half months now
+                  and{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    the image is crystal clear
+                  </span>
+                  , never had a problem on the case comparing my old case. The
+                  image started fading into yellow-ish color after a couple
+                  weeks. Really recommended 👍"
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
-                <img className="rounded-full h-12 w-12 object-cover" src="/users/user-1.png" alt="user" />
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
                 <div className="flex flex-col">
                   <p className="font-semibold">Arif W</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
@@ -166,12 +178,21 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                I dropped my phone a week ago and luckily it wasn&apos;t damaged. It&apos;s really worth to buy cuz you don't really know when bad things happen.{" "}
-                  <span className="p-0.5 bg-slate-800 text-white">The detail is superb</span>, I will recommend to my fellow definitely..."
+                  I dropped my phone a week ago and luckily it wasn&apos;t
+                  damaged. It&apos;s really worth to buy cuz you don't really
+                  know when bad things happen.{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    The detail is superb
+                  </span>
+                  , I will recommend to my fellow definitely..."
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
-                <img className="rounded-full h-12 w-12 object-cover" src="/users/user-3.png" alt="user" />
+                <img
+                  className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-3.png"
+                  alt="user"
+                />
                 <div className="flex flex-col">
                   <p className="font-semibold">Karina</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
@@ -184,9 +205,70 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
 
+        {/* testimonials */}
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2x sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{" "}
+                <span className="relative px-2 bg-cyan-600 text-white">
+                  your own case{" "}
+                </span>{" "}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="w-5 h-5 text-cyan-600 inline mr-1.5" />
+              High-quality sillicone material
+            </li>
+            <li className="w-fit">
+              <Check className="w-5 h-5 text-cyan-600 inline mr-1.5" />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="w-5 h-5 text-cyan-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="w-5 h-5 text-cyan-600 inline mr-1.5" />
+              5 year print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link className={buttonVariants({
+                size: 'lg',
+                className: 'mx-auto mt-8'
+              })} href="/configure/upload">Create your case now <ArrowRight className="h-4 w-4 ml-1.5" /></Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
