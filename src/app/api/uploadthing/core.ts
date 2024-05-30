088609e653816fd317a513f49c1e0fac67edc387
wibawaarif <arifwb.work@gmail.com>
2024-05-30 21:35:01 +0700

    add upload thing core

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
+import sharp from "sharp"
+import { db } from "@/db";
  
 const f = createUploadthing();
  
@@ -14,8 +16,34 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
         const {configId} = metadata.input
  
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { configId };
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          }
+        })
+
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId
+          },
+          data: {
+            croppedImageUrl: file.url,
+          }
+        })
+
+        return { configId: updatedConfiguration.id}
+      }
     }),
 } satisfies FileRouter;
  
